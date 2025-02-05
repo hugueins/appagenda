22e26834fd20d344c4dfcd63ad1acec56f18b2c0
Hugo Alfredo Herrera Villalva <hugo.herrera@sercop.gob.ec>
2025-02-04 21:11:51 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appAgenda',
+  webDir: 'www'
+};
+
+export default config;
